a4773ab8d1a9718af869597eb7853b294804e208
AlexNaltu <naltuok@gmail.com>
2023-09-16 11:44:04 +0200

    update

--- src/components/Special.tsx
+++ src/components/Special.tsx
@@ -27,7 +27,7 @@ export default async function Special() {
       <div className="my-20 flex flex-col gap-8">
         <div className="flex flex-col items-center gap-1">
           <h1 className="text-2xl lg:text-3xl xl:text-4xl text-orange-300 font-bold ">Today Special</h1>
-          <p className="text-white font-extralight text-xs md:text-base lg:text-lg">these are today's special meals & desert</p>
+          <p className="text-white font-extralight text-xs md:text-base lg:text-lg">these are today special meals & desert</p>
         </div>
         <div>
           <ul className="flex flex-col items-center sm:flex-row justify-center gap-4">
